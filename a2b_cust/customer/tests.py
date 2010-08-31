2a83d78888e60d73ec01e5ac6f0429f521522a5f
Shrenik Patel <shrenik181986@gmail.com>
2010-08-31 17:52:32 +0530

    Unittest Case for Language model (completed)

--- a2b_cust/customer/tests.py
+++ a2b_cust/customer/tests.py
@@ -1,7 +1,7 @@
 import urllib, base64
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
-from a2b_cust.customer.models import *
+from a2b_cust.customer.models import Language
 from function_def import my_func
 
 
@@ -18,29 +18,26 @@ class LanguageTestCase(TestCase):
         auth = auth.strip()
         self.extra = {
             'HTTP_AUTHORIZATION': auth,
+            'HTTP_X_REQUESTED_WITH':'XMLHttpRequest'
         }
         self.dutch = Language.objects.create(code="Du", name="Dutch", lname="Dutch", charset="UTF-8")
 
     def test_get(self):
         response = self.client.get('/api/language', {}, **self.extra)
         self.assertEqual(response.status_code, 200)
-    
-    """
-    def test_post(self):
-        r = self.client.post('/api/language/', {'code': 'Ur', 'name': 'Urdu' ,'lname': 'Urdu' ,'charset': 'UTF-8', },content_type="text/xml")
-        try :
-            self.assertEqual(r.status_code, 200)
-        except :
-            self.assertEqual(r.status_code, 401)
 
     def test_put(self):
-        r = self.client.put('/api/language/Ur/', {'name': 'Urdu'})
-        self.assertEqual(r.status_code, 200)
-
+        response = self.client.put('/api/language/Du/', {'name': 'dutch'}, **self.extra)
+        self.assertEqual(response.status_code, 200)
+        
     def test_delete(self):
-        r = self.client.delete('/api/language/Ur/', {})
-        self.assertEqual(r.status_code, 200)
-    """
+        response = self.client.delete('/api/language/Du/', {}, **self.extra)
+        self.assertEqual(response.status_code, 204)
+
+    def test_post(self):               
+        response = self.client.post('/api/language', { 'name':'Urdu' ,'code':'Ur','lname':'Urdu' ,'charset':'UTF-8'} , **self.extra)
+        self.assertEqual(response.status_code, 200)
+
 
 
 """
